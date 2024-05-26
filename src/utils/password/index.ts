7c30e85575936ded9c488238fa5cfddc54613a45
taroj1205 <taroj1205@gmail.com>
2024-05-26 12:30:45 +1200

    feat: added amount

--- src/utils/password/index.ts
+++ src/utils/password/index.ts
@@ -8,17 +8,22 @@ export const generatePassword = (searchParams: URLSearchParams): string => {
 	const includeSymbols = searchParams.get("symbols") !== "false";
 	const includeNumbers = searchParams.get("numbers") !== "false";
 	const includeUpperCase = searchParams.get("upper") !== "false";
+	const amount = Number(searchParams.get("amount")) || 1;
 
 	let passwordCharacters = lowerCaseLetters;
 	if (includeSymbols) passwordCharacters += symbols;
 	if (includeNumbers) passwordCharacters += numbers;
 	if (includeUpperCase) passwordCharacters += upperCaseLetters;
 
-	let password = "";
-	for (let i = 0; i < length; i++) {
-		const randomIndex = Math.floor(Math.random() * passwordCharacters.length);
-		password += passwordCharacters[randomIndex];
+	const passwords = [];
+	for (let j = 0; j < amount; j++) {
+		let password = "";
+		for (let i = 0; i < length; i++) {
+			const randomIndex = Math.floor(Math.random() * passwordCharacters.length);
+			password += passwordCharacters[randomIndex];
+		}
+		passwords.push(password);
 	}
 
-	return password;
+	return passwords.join("\n");
 };
